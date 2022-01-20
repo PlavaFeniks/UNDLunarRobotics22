ae8842c7f8d867565af28ce82d46fc8a495fadb9
Alex E <alexandere42@outlook.com>
2022-01-20 15:29:43 -0600

    good question

---
+++ CMakeFiles/testbed.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testbed.dir/testbed.cpp.o"
+  "robotCode/bin/testbed"
+  "robotCode/bin/testbed.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testbed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
