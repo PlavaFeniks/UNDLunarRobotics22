2cfff22b3282a38ef8876dd7df80273633ba6729
SoggyOtter <alexandere42@outlook.com>
2022-01-24 14:57:22 -0600

    Fixed more config shtuff

--- install.sh
+++ install.sh
@@ -4,25 +4,31 @@
 
 apt-get update
 
-
+export temp_home=$(pwd)
 
 #install Opencv
-apt-get install -y cmake g++ wget unzip
-wget -O opencv.zip https://github.com/opencv/archive/4.x.zip ~/Downloads
+sudo apt-get install -y cmake g++ wget unzip
+cd ~/Downloads
+wget -O ~/Downloads/opencv.zip https://github.com/opencv/opencv/archive/4.x.zip
 unzip  ~/Downloads/opencv.zip
+
 mkdir -p ~/Downloads/build && cd ~/Downloads/build
 cmake ../opencv-4.x
 cmake --build
+#return to original place 
 #install sdl2
-apt-get install libsdl2-2.0
+sudo apt-get install libsdl2-2.0
 
 
 
 #pipe input to  /etc/networks/interface file
-cat pi/interfaces.d >> /etc/network/interfaces
-cp pi/robo.service /etc/systemd/system/
-systemctl daemon-reload && systemctl enable robo.service && systemctl start robo.service
-apt-get updgrade
+
+sudo cp $temp_home/pi/interfaces.d  /etc/network/
+sudo cp $temp_home/pi/robo.service /etc/systemd/system/
+sudo systemctl daemon-reload && sudo systemctl enable robo.service && sudo systemctl start robo.service
+sudo apt-get upgrade
+
+echo temp_home=""
 
 echo "All required dependencies installed !!!\n"
 echo "Enjoy"
