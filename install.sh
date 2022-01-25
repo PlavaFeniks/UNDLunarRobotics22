71bf97be5132c2b8b5c2374207be5ab9464e6dd8
Alex E <alexandere42@outlook.com>
2022-01-25 14:17:55 -0600

    Final fix for install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@
 
 apt-get update
 
-
+export temp_home= $(pwd)
 
 #install Opencv
-apt-get install -y cmake g++ wget unzip
-wget -O opencv.zip https://github.com/opencv/archive/4.x.zip ~/Downloads
+sudo apt-get install -y cmake g++ wget unzip
+wget -O ~/Downloads/opencv.zip https://github.com/opencv/opencv/archive/4.x.zip
 unzip  ~/Downloads/opencv.zip
 mkdir -p ~/Downloads/build && cd ~/Downloads/build
 cmake ../opencv-4.x
@@ -19,9 +19,9 @@ apt-get install libsdl2-2.0
 
 
 #pipe input to  /etc/networks/interface file
-cat pi/interfaces.d >> /etc/network/interfaces
-cp pi/robo.service /etc/systemd/system/
-systemctl daemon-reload && systemctl enable robo.service && systemctl start robo.service
+sudo cp $temp_home/pi/interfaces /etc/network/interfaces
+sudo cp $temp_home/pi/robo.service /etc/systemd/system/
+sudo systemctl daemon-reload && sudo systemctl enable robo.service && sudo systemctl start robo.service
 apt-get updgrade
 
 echo "All required dependencies installed !!!\n"
