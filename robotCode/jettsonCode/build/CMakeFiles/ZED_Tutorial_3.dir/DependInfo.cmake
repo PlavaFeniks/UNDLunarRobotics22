bdda6640313d7005665e91bd36bc2d340a0fd1dd
BigBoiJetson <marcok1221@gmail.com>
2022-02-08 14:13:56 -0600

    added jetson stuff

---
+++ robotCode/jettsonCode/build/CMakeFiles/ZED_Tutorial_3.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rmc/Desktop/code/pointCloudJohn/cpp/main.cpp" "/home/rmc/Desktop/code/pointCloudJohn/cpp/build/CMakeFiles/ZED_Tutorial_3.dir/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda-10.2/include"
+  "/usr/local/zed/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
