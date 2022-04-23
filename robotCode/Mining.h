61e19a5006509f0266a6e2529fbca681fe9fe0d0
Alex E <60448790+SoggyOtter@users.noreply.github.com>
2022-04-23 13:13:09 -0500

    Changes with mining and GPIO libraries (#33)
    Co-authored-by: PlavaFeniks <marcok1221@gmail.com>

--- robotCode/Mining.h
+++ robotCode/Mining.h
@@ -1,15 +1,69 @@
 
+#include <iostream>
+// for delay function.
+#include <chrono>
+#include <map>
+#include <string>
+#include <thread>
 
+// for signal handling
+#include <signal.h>
+#include "infra/readSerial.h"
+#include <JetsonGPIO.h>
+#define LSwitch 18 // Switch repersenting digger is fully lowered
+#define HSwitch 22// Switch representing digger is fully raised
 
-
-  
-#define LSwitch 0 // Switch repersenting digger is fully lowered
-#define HSwitch 7// Switch representing digger is fully raised
-
+using namespace GPIO;
 using namespace std;
 
+void LimitSwitchTest(){
 
 
+	GPIO::cleanup();
+    //signal(SIGINT, signalHandler);
+    GPIO::setmode(GPIO::BOARD);
+
+    GPIO::setup(LSwitch, GPIO::IN);
+    GPIO::setup(HSwitch, GPIO::IN);
+
+/*
+    wiringPiSetup();
+    pinMode(HSwitch, INPUT);  // twlls gpio pins what is gettign pluged in
+    pinMode(LSwitch, INPUT);
+
+    pullUpDnControl(HSwitch, PUD_DOWN); // sets pull down resistors
+    pullUpDnControl(LSwitch, PUD_DOWN);
+    */
+    int switchL;
+    int switchH;
+    
+    
+    
+while(true){
+	
+	
+	switchL = GPIO::input(LSwitch);
+	switchH = GPIO::input(HSwitch);
+
+    if (switchL == 0){
+
+    cout<<"Lowered"<<endl;
+    //return;
+    }
+    else if (switchH == 0){
+   
+    cout<<"Raised"<<endl;
+    std::_Exit(1);
+    //return;
+    }
+    else {
+        cout<<"neither Pressed" << endl;
+    }
+
+}
+}
+
+/*
 void MiningTime1(readSerial* ampSerial){
     float *arr = (float*) malloc(sizeof(float) * 10);
     
@@ -17,20 +71,16 @@ void MiningTime1(readSerial* ampSerial){
     chrono::steady_clock::time_point start = chrono::steady_clock::now();
     string amps;
     
-    wiringPiSetup();
-    pinMode(HSwitch, INPUT);  // twlls gpio pins what is gettign pluged in
-    pinMode(LSwitch, INPUT);
+   // wiringPiSetup();
 
-    pullUpDnControl(HSwitch, PUD_DOWN); // sets pull down resistors
-    pullUpDnControl(LSwitch, PUD_DOWN);
 
     char  x;  // X button input
     float IBuckets;  // Current input
     float  Ie  = 1100; // Current epected
     
-    int BucketSpeed = 0; // quad speed = .1/60*4096*RPM/gearbox. Efficiency speed is 12150 rpm btw
+    int BucketSpeed = 300; // quad speed = .1/60*4096*RPM/gearbox. Efficiency speed is 12150 rpm btw
     //int buckScrewRatio = 5; //number of revolution of bucket shaft to revolutions of screw shaft
-    int ScrewSpeed = 1000; //relation between bucket speed and screw speed
+    int ScrewSpeed = -1000; //relation between bucket speed and screw speed
     //float ScrewSpeed = .5;
     int maxLoad = 30000;
 
@@ -55,10 +105,14 @@ void MiningTime1(readSerial* ampSerial){
     PID_valsScrew[PID_D] = .5;
     PID_valsScrew[PID_F] = .04;
 
+    int loopCount = 0;
 
     TalonPair* buckets = new TalonPair(6,VELOCITY,limits, PID_valsBuck);
    
     TalonPair* screwdriver= new TalonPair(5,VELOCITY,limits, PID_valsScrew);
+
+
+    
     //TalonPair * screwdriver = new TalonPair(5);
     //screwdriver->INVERT();
     //TalonPair * buckets = new TalonPair(6);
@@ -69,26 +123,52 @@ void MiningTime1(readSerial* ampSerial){
         
             buckets->SETSPEED(BucketSpeed); // set speed for the two motors
             screwdriver->SETSPEED(ScrewSpeed); 
+
             arr = ampSerial->getSerialVals(10);
+
+            chrono::steady_clock::time_point start = chrono::steady_clock::now();
+            chrono::steady_clock::time_point end = chrono::steady_clock::now();
+        
+            if (loopCount % 5000 == 0){
+            arr = ampSerial->getSerialVals(10);
+            cout<<"checked"<< endl;
+            break;
+            }
+            loopCount++;
+        
+
             IBuckets = arr[8];
             
             float Regolith = (arr[0]+arr[1]+arr[2]+arr[3])/4.0f;
-            
 
+
+
+            if (digitalRead(!HSwitch)){
+                while(true){
+                        screwdriver->SETSPEED(0);
+                        buckets->SETSPEED(0);
+                        cout<<"unexpected Halt" <<endl;
+                        return; 
+                        
+
+                    }
+            }
             // if the lower switch is pressed
-            if (digitalRead(LSwitch)){
+            else if (digitalRead(!LSwitch)){
 
                 while(true)
                 {
                     
-                    screwdriver->SETSPEED(-ScrewSpeed);
+                    screwdriver->SETSPEED(-1.5*ScrewSpeed);
                     buckets->SETSPEED(BucketSpeed);
                     
-                    if (digitalRead(HSwitch)){
+                    if (digitalRead(!HSwitch)){
                         screwdriver->SETSPEED(0);
                         buckets->SETSPEED(0);
                         //return 0; mining complete
-
+                        
+                        return;
+                        
                     }
                     else {
                         //cout << "Finishing due to limit switch";
@@ -101,10 +181,15 @@ void MiningTime1(readSerial* ampSerial){
             //else if((cin >> I) >= Ie){
             else if(IBuckets >= Ie){
                 while (true){
+                    
+
+                    arr = ampSerial->getSerialVals(10);
+                    IBuckets = arr[8];
+
 
                     screwdriver->SETSPEED(-ScrewSpeed);
 
-                    if (digitalRead(HSwitch)){
+                    if (digitalRead(!HSwitch)){
                         screwdriver->SETSPEED(0);
                         buckets->SETSPEED(0);
                         
@@ -126,7 +211,7 @@ void MiningTime1(readSerial* ampSerial){
                     screwdriver->SETSPEED(-ScrewSpeed);
                     buckets->SETSPEED(BucketSpeed);
                     
-                    if (!digitalRead(HSwitch)){
+                    if (!digitalRead(!HSwitch)){
                         screwdriver->SETSPEED(0);
                         buckets->SETSPEED(0);
                         return;
@@ -141,13 +226,13 @@ void MiningTime1(readSerial* ampSerial){
             
         }
     }
-    //return 0;
+    return;
 }
 
-/*
-dead function
+/////////////////////
+//dead function
 // this function averages the reading of the load cells
-
+/*
 float hopperLoad(readSerial* ampSerial){
     
     
@@ -157,4 +242,30 @@ float hopperLoad(readSerial* ampSerial){
 
     return Regolith;
 }
+* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+* 
 */
